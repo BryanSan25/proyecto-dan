3f3c3a318768442dcd7041926f11f2c28036215e
Bryan Sanchez <83999131+BryanSan25@users.noreply.github.com>
2022-01-12 02:33:41 -0500

    Add files via upload

---
+++ proyecto new 1/js/main.js
@@ -0,0 +1,9 @@
+
+function closePopUp(){
+
+    document.getElementById('containerPopUp').style.display = 'none';
+    
+}
+function displayIS(){
+    document.getElementById('containerPopUp').style.display = 'block';
+}
